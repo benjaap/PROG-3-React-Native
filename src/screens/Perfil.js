319055edbb919a61edf1a83971f9897505b9d3d7
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-11-07 09:24:37 -0300

    Agregando cosas al registro

--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import { auth } from '../firebase/config';
 import { Text, TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default class Perfil extends Component{
+export default class Perfil extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+            email:'',
+            password:'' 
+        }
+    }
+    logOut (){
+        auth.signOut(); 
+        this.props.navigation.navigate("Register")
+    }
 
     render(){
         return(
-            <View>
-                <Text> Mi perfil</Text>
-            </View>
-        )
+             <View>
+                <Text>Mi perfil</Text>
+                <TouchableOpacity onPress={()=> this.logOut()}>
+                    <Text>Cerrar Sesión</Text>
+                </TouchableOpacity>
+            </View>)
+       
     }
 }
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,22 +1,107 @@
-import React, {Component} from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { db, auth } from '../firebase/config';
+import firebase from "firebase";
 
 
 
-class Regiter extends Component {
-    constructor(props){
+
+class Register extends Component {
+    constructor(props) {
         super(props)
-        
+        this.state = {
+            email: '',
+            password: '',
+            username: '',
+            err: '',
+            post: '',
+            bio: '',
+
+
+
+        }
     }
-    render(){
+
+    componentDidMount(){
+        auth.onAuthStateChanged((user)=> {this.props.navigation.navigate("Menu")})
+    }
+
+    
+
+    register(email, password, username, bio, post) {
+        auth.createUserWithEmailAndPassword(email, password)
+        .then( (res)=> {
+            db.collection('users').add ({
+                email: email,
+                username: username,
+                bio: bio,
+                post: []
+            })   
+            .then((res)=> {
+                this.setState({
+                    email: "",
+                    password: "",
+                    username: "",
+                    bio:"",
+                });
+                this.props.navigation.navigate("Menu");
+            }) }
+            )
+            .catch(err => this.setState({err: err.message}) )
+
+
+    }
+
+   
+
+
+    render() {
         return (
             <>
-                <Text>Soy la pagina de Registro</Text>
-    
-                <TouchableOpacity
-                    onPress = {()=> this.props.navigation.navigate('login')} >
-                    <Text>Te dirijo para loguearte</Text>
-                </TouchableOpacity>
+                <View>
+                    <Text>Registro</Text>
+                </View>
+                <View>
+                    <TextInput
+                        style={style.campo}
+                        placeholder='email'
+                        keyboardType="email-addres"
+                        onChangeText={userEmail => this.setState({ email: userEmail })}
+                        value={this.state.email}
+                    />
+                    <TextInput
+                        style={style.campo}
+                        placeholder='contraseña'
+                        keyboardType="default"
+                        secureTextEntry='true'
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password: password})}
+                    />
+                    <TextInput
+                        style={style.campo}
+                        placeholder=' Nombre de Usuario'
+                        keyboardType="default"
+                        value={this.state.username}
+                        onChangeText={username => this.setState({ username: username })}
+                    />
+                    <TextInput
+                        style={style.campo}
+                        placeholder='Biografia'
+                        keyboardType="default"
+                        value={this.state.bio}
+                        onChangeText={bio => this.setState({ bio: bio })}
+                    />
+                </View>
+                <View>
+
+                    <TouchableOpacity onPress={() => { this.register(this.state.email, this.state.password, this.state.username, this.state.bio, this.state.post) }}>
+                        <Text>Registrame</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+                        <Text> Ya tengo una cuenta</Text>
+                    </TouchableOpacity>
+                    <Text>{this.state.err}</Text>
+                </View>
             </>
         );
 
@@ -26,4 +111,19 @@ class Regiter extends Component {
     }
 }
 
-export default Regiter;
+const style = StyleSheet.create ({
+    campo: {
+        fontSize: 18,
+        borderColor: 'red',
+        borderEndWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        marginVertical: 8,
+        marginHorizontal: 16
+        
+    }
+
+
+})
+
+export default Register;
