a8522fe05297b844534963ef65077c700972b92b
benjaap <89359576+benjaap@users.noreply.github.com>
2022-11-07 10:39:52 -0300

    login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,21 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { auth } from '../firebase/config';
 
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      pass: '',
+    };
+  }
+  loginUser(email, pass) {
+    auth
+      .signInWithEmailAndPassword(email, pass)
+      .then((res) => {
+        this.props.navigation.navigate('Menu')
+      })
+      .catch(err => this.setState({err: err.message}) )
+    }
 
-
-function Login() {
+  render() {
     return (
-      <>
-      <Text>Soy la pagina para  loguearte</Text>
-
-      
-      
-      
-      </>
+      <View>
+        <Text>Ingresar</Text>
+        <View>
+          <TextInput style={styles.field} placeholder="email" keyboardType="email-address" onChangeText={(text) => this.setState({ email: text })} value={this.state.email} />
+          <TextInput style={styles.field} placeholder="password" keyboardType="default" secureTextEntry onChangeText={(text) => this.setState({ pass: text })} value={this.state.pass} />
+          <Text onPress={() => this.loginUser(this.state.email, this.state.pass)}>Loguearme</Text>
+          <Text onPress={() => this.props.navigation.navigate('Register')}>Crear una cuenta</Text>
+        </View>
+      </View>
     );
   }
+}
 
-  export default Login;
+const styles = StyleSheet.create({
+  field: {},
+});
 
-
-  
+export default Login;
