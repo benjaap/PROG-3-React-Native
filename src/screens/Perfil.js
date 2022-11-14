27707fba2c172b65f29e8205b10c06fe86e23361
benjaap <89359576+benjaap@users.noreply.github.com>
2022-11-14 10:58:29 -0300

    Username/mail/utlima fecha de acceso en pagina Mi perfil

--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -1,29 +1,74 @@
 import React, { Component } from 'react'
-import { auth } from '../firebase/config';
+import { auth, db } from '../firebase/config';
 import { Text, TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Perfil extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            email:'',
-            password:'' 
+        this.state = {
+            email: '',
+            username: '',
+            post: [],
         }
     }
-    logOut (){
-        auth.signOut(); 
-        this.props.navigation.navigate("Register")
+
+    componentDidMount() {
+        db.collection('users')
+            .where('email', '==', auth.currentUser.email)
+            .onSnapshot(
+                docs => {
+                    let post = [];
+                    docs.forEach(doc => {
+                        post.push({
+                            id: doc.id,
+                            data: doc.data()
+                        })
+                        this.setState({
+                            username: post[0].data.username,
+                            loading: false
+                        })
+                    })
+                }
+            )
+
+        db.collection('posts')
+            .where('owner', '==', auth.currentUser.email)
+            .onSnapshot(
+                docs => {
+                    let post = [];
+                    docs.forEach(doc => {
+                        post.push({
+                            id: doc.id,
+                            data: doc.data()
+                        })
+                        this.setState({
+                            posts: post,
+                            loading: false
+                        })
+                    })
+                }
+            )
     }
 
-    render(){
-        return(
-             <View>
-                <Text>Mi perfil</Text>
-                <TouchableOpacity onPress={()=> this.logOut()}>
-                    <Text>Cerrar Sesión</Text>
-                </TouchableOpacity>
-            </View>)
-       
-    }
+logOut() {
+    auth.signOut();
+    this.props.navigation.navigate("Register")
+}
+
+render() {
+    return (
+        <View>
+            <Text>Mi perfil</Text>
+            <TouchableOpacity onPress={() => this.logOut()}>
+                <Text>Cerrar Sesión</Text>
+            </TouchableOpacity>
+            <text>{this.state.username}</text>
+            <text>{auth.currentUser.email}</text>
+            {/* <text>{this.state.posts.lenght}</text> */}
+            <text>{auth.currentUser.metadata.lastSignInTime}</text>
+            
+
+        </View>)
+}
 }
