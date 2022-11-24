2c2db008cd2fb2a9cfecc17e528db788fe3d5905
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-11-23 23:51:40 -0300

    Se me fue un detalle

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -21,7 +21,7 @@ class Register extends Component {
     }
 
     componentDidMount(){
-        auth.onAuthStateChanged((user)=> {this.props.navigation.navigate('Register')})
+        auth.onAuthStateChanged((user)=> {this.props.navigation.navigate('Menu')})
     }
 
     
